b6bb41c322dab71b37df8ecb507868ee5c43a7c3
Felipe Vasquez <fvasquez.1288@gmail.com>
2026-02-02 01:21:20 -0300

    Agregar estructura Maven, código fuente y modificar 07_check_db.sql

---
+++ database/07_check_db.sql
@@ -0,0 +1,13 @@
+-- Mostrar bases de datos
+SHOW DATABASES;
+
+-- Seleccionar la base de datos
+USE AlkeWallet;
+
+-- Ver tablas
+SHOW TABLES;
+
+-- Revisar datos
+SELECT * FROM usuario;
+SELECT * FROM moneda;
+SELECT * FROM transaccion;
